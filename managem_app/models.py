863a6ff618f1e3e68b10781bdd449c1ac866bbe9
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-06-09 17:48:45 -0400

    update

--- managem_app/models.py
+++ managem_app/models.py
@@ -2,29 +2,31 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    group = models.CharField(max_length=250)
+   
+    def __str__(self):
+        return self.group
+
 class Subscription(models.Model):
     name = models.CharField(max_length=250)
     due_date = models.DateField(auto_now_add=True)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
     frequency = models.CharField(max_length=250)
     image_url = models.TextField(blank=True)
+    subscribe = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subscriptions')
 
     def __str__(self):
         return self.name
 
-class Category(models.Model):
-    group = models.CharField(max_length=250)
-    subscribe = models.ForeignKey(Subscription, on_delete=models.CASCADE, related_name='kind')
 
-    def __str__(self):
-        return self.group
 
 class History(models.Model):
     date = models.DateField(auto_now_add=True)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
     payment_type = models.CharField(max_length=250)
     total_amt = models.DecimalField(max_digits=5, decimal_places=2)
-    name = models.ForeignKey(Subscription, on_delete=models.CASCADE, related_name='record')
+    name = models.ForeignKey(Subscription, on_delete=models.CASCADE, related_name='records')
 
     def __str__(self):
-        return self.total_amt
+        return str(self.name)
