68281f377362f44154331b33e53577b1b6f3cdd9
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-06-10 10:46:04 -0400

    created views

--- managem_app/views.py
+++ managem_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import HistorySerializer, SubscriptionSerializer, CategorySerializer
+from .models import History, Subscription, Category
 
 # Create your views here.
+
+
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SubscriptionView(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+
+class HistoryView(viewsets.ModelViewSet):
+    queryset = History.objects.all()
+    serializer_class = HistorySerializer
+
+
+
+
+
+
