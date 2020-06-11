169677b869b2994753aae4437f5a2bfd209035e9
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-06-11 09:08:16 -0400

    updated page. tried  to add if/else statement

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
     state = {
-        categories: [],
-        isFree: false
+        categories: []
     }
 
     componentDidMount() {
@@ -27,20 +26,6 @@ export default class Home extends Component {
         }
     }
 
-    loadFree = async () => {
-        const isFree = !this.state.isFree
-        this.setState({ isFree })
-        try {
-
-            const res = await axios.get('/api/v1/category/')
-            const newState = { ...this.state }
-            newState.categories = res.data
-            this.setState(newState)
-        } catch (err) {
-            console.log('Error getting all categories')
-            console.log(err)
-        }
-    }
 
 
     render() {
@@ -50,24 +35,25 @@ export default class Home extends Component {
             <div>
                 <h2>Home Page</h2>
                 <button>Add Subscription</button>
-                { categories.map((item, i) => {
+                { categories.map((item) => {
                     return (
-                        <div key={ `${item.i}` }>
-                            {/* if group is free, load data w/name and image. If it's paid, load all data */}
-                            { item.group === 'free' ? 'this is free'
-                                :
-                                <h3>{ item.group === 'paid' }</h3> }
+                        <div key={ `${item.id}` }>
+                            
+                            <h3>{ item.group }</h3>
                             { item.subscriptions.map((subs) => {
                                 return (
                                     <div key={ `${subs.id}` }>
-                                        {/* if category is free, only print the image and the name */ }
 
                                         <Link to={ `/subscription/${subs.id}` }>
                                             <img src={ subs.image_url } width="100" height="75" />
                                             <div>{ subs.name }</div>
-                                            <div>{ subs.due_date }</div>
-                                            <div>{ subs.amount }</div>
-                                            <div>{ subs.frequency }</div>
+                                            {/* if group is paid, show date/amt,freq..if free show only name and img */}
+                                            { item.group === 'paid' ?
+                                                <div>
+                                                    <div>{ subs.due_date }</div>
+                                                    <div>{ subs.amount }</div>
+                                                    <div>{ subs.frequency }</div>
+                                                </div> : null }
                                         </Link>
                                     </div>
 
