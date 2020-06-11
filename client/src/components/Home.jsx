2c5ca63f0c9f3ab36518ab980795e1a1282e7214
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-06-10 21:09:58 -0400

    rdebugged error w/subscription object

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,12 +4,12 @@ import axios from 'axios'
 export default class Home extends Component {
     state = {
         categories: [],
-        subscriptions: [],
+        subscriptions: {},
     }
 
     componentDidMount() {
         this.getAllCategories()
-        this.getAllSubscriptions()
+
     }
 
 
@@ -19,6 +19,7 @@ export default class Home extends Component {
             const res = await axios.get('/api/v1/category/')
             const newState = { ...this.state }
             newState.categories = res.data
+            newState.subscriptions = res.data.subscriptions
             this.setState(newState)
         } catch (err) {
             console.log('Error getting all subscriptions')
@@ -26,40 +27,31 @@ export default class Home extends Component {
         }
     }
 
-    getAllSubscriptions = async () => {
-
-        try {
-            const res = await axios.get('/api/v1/subscription/')
-            const newState = { ...this.state }
-            newState.subscriptions = res.data
-            this.setState(newState)
-        } catch (err) {
-            console.log('Error getting all subscriptions')
-            console.log(err)
-        }
-    }
 
     render() {
         const { subscriptions, categories } = this.state
 
-        console.log(categories)
+        console.log(categories[0])
         return (
             <div>
                 <h2>Home Page</h2>
-                { categories.map((item) => {
+                { categories.map((item, i) => {
                     return (
-                        <div>
+                        <div key={ `${item.id}` }>
                             <h3>{ item.group }</h3>
-                            <div>{item.subscriptions}</div>
-                            { subscriptions.map((item) =>
-                                <div>
-                                    <img src={ item.image_url } width="100" height="80" alt="subscription image" />
-                                    <div> Name: { item.name }</div>
-                                    <div>{ item.due_date }</div>
-                                    <div>{ item.amount }</div>
-                                    <div>{ item.frequency }</div>
-                                </div>
-                            ) }
+                            { item.subscriptions.map((subs) => {
+                                return (
+                                    <div>
+                                        <img src={ subs.image_url } width="100" height="75" />
+                                        <div>{ subs.name }</div>
+                                        <div>{ subs.due_date }</div>
+                                        <div>{ subs.amount }</div>
+                                        <div>{ subs.frequency }</div>
+                                    </div>
+
+                                )
+                            }) }
+
                         </div>
                     )
                 }) }
